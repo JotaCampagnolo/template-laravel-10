abf3d21d7a254a8a5a142239ea81db36dd4ce65f
João M. Campagnolo <joao@amplimed.com.br>
2023-06-07 18:23:33 -0300

    feat: new page with logout for the login starter template.

--- resources/views/protected.blade.php
+++ resources/views/protected.blade.php
@@ -7,6 +7,12 @@
             <h1 class="text-success">You're In</h1>
             <h2>A Protected Page</h2>
             <p class="mt-3 font-200 text-justify">This page is protected by authentication. If you can see this page it means you're authenticated.</p>
+            <form action="{{ url('logout') }}" method="POST">
+                @csrf()
+                <button type="submit" class="btn btn-danger">
+                    Logout
+                </button>
+            </form>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
 Route::get('/login', [LoginController::class, 'show'])->name('login.show');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+Route::post('/logout', [LoginController::class, 'logout'])->name('login.logout');
 Route::get('/protected', function () {
     return view('protected');
-});
+})->middleware('auth');
